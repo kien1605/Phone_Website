70e9cf39d6a63ecf9ed17253e737fc5aa28121bd
kien.nguyen <kien.nguyen@kyanon.digital>
2021-07-30 08:33:19 +0700

    Initial app

---
+++ Demo/Resource/js/index.js
@@ -0,0 +1,12 @@
+// Navigation
+
+// Pop up
+
+// Fixed navigation
+
+// smooth navigation
+
+// fix nav-bar
+
+// AOS
+AOS.init();
---
+++ Website/resources/js/scripts.js
@@ -0,0 +1,51 @@
+// $(document).ready(
+//     function(){
+//         //$('h1').css('color','red')
+//         $('h1').click(
+//             function(){
+//                 $(this).css('color','red');
+//             }
+//         )
+//         //alert('Hello')//hiển thị message box lên khi chạy server
+//     }
+// )
+$(document).ready(
+    function(){
+
+        // sticky nav
+        $('.about-section').waypoint(
+            function(direction){
+                if(direction == "down"){
+                    $('nav').addClass('sticky');
+                }else {
+                    $('nav').removeClass('sticky');
+                }
+            },{
+                offset: '600px'
+            }
+        )
+
+        // Scroll
+        $('a').click(function(event){
+            $('html, body').animate({
+                scrollTop: $( $.attr(this, 'href') ).offset().top
+            }, 700);
+            event.preventDefault();
+        });
+
+        // mobile navigation
+        $('.mobile-nav-icon').click(
+            function(){
+                if($('.mobile-nav-icon').hasClass('fa-bars')){
+                    $('.mobile-nav-icon').addClass('fa-times');
+                    $('.mobile-nav-icon').removeClass('fa-bars')
+                }
+                else{
+                    $('.mobile-nav-icon').addClass('fa-bars');
+                    $('.mobile-nav-icon').removeClass('fa-times')
+                }
+                $('.main-nav').slideToggle(200);
+            }
+        )
+    }
+)
